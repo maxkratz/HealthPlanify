70e8066341cebedc9d5226624fe6937b0f45ecc9
Néstor Feliciano <alu0101488998@ull.edu.es>
2025-02-22 19:26:18 +0000

    Frotend Initial Config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+    // const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div className='bg-blue-500 text-white p-1'>
+                TAILWIND
+            </div>
+        </>
+    )
+}
+
+export default App
