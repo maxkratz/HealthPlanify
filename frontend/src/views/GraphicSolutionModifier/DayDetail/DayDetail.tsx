e9484597cb7aa7ed490af1ed55bae64a777ed636
Néstor Feliciano <alu0101488998@ull.edu.es>
2025-04-06 15:14:09 +0100

    Legend

--- frontend/src/views/GraphicSolutionModifier/DayDetail/DayDetail.tsx
+++ frontend/src/views/GraphicSolutionModifier/DayDetail/DayDetail.tsx
@@ -13,28 +13,28 @@ export const DayDetail: React.FC<DayDetailProps> = ({ day }) => {
     }
 
     return (
-        <div className='flex flex-col gap-4'>
-            <h3>Day {day} Details</h3>
+        <div className='flex flex-col gap-6'>
+            <h3 className='text-center'>Day {day} Details</h3>
 
-            <div className='flex flex-col gap-2'>
-                <h4>Surgeons</h4>
+            <div className='flex flex-col gap-2 text-left'>
+                <h4 className='text-center'>Surgeons</h4>
                 {inputData.surgeons.map((surgeon) => (
                     <div key={surgeon.id}>
                         <strong>ID:</strong> {surgeon.id} - <strong>Max Surgery Time:</strong>{" "}
                         {surgeon.max_surgery_time[day] !== undefined
-                            ? `${surgeon.max_surgery_time[day]} minutes`
+                            ? `${surgeon.max_surgery_time[day]}`
                             : 'N/A'}
                     </div>
                 ))}
             </div>
 
-            <div className='flex flex-col gap-2'>
-                <h4>Operating Theaters</h4>
+            <div className='flex flex-col gap-2 text-left'>
+                <h4 className='text-center'>Operating Theaters</h4>
                 {inputData.operating_theaters.map((ot) => (
                     <div key={ot.id}>
                         <strong>ID:</strong> {ot.id} - <strong>Availability:</strong>{" "}
                         {ot.availability[day] !== undefined
-                            ? `${ot.availability[day]} minutes`
+                            ? `${ot.availability[day]}`
                             : 'N/A'}
                     </div>
                 ))}
--- frontend/src/views/GraphicSolutionModifier/PatientDetail/PatientDetail.tsx
+++ frontend/src/views/GraphicSolutionModifier/PatientDetail/PatientDetail.tsx
@@ -19,30 +19,45 @@ export const PatientDetail: React.FC<PatientDetailProps> = ({ patientId }) => {
 
     return (
         <div className='flex flex-col gap-2'>
-            <h3>{patientId} Patient Details</h3>
-            <p><strong>Admission Day:</strong> {patientFull.admission_day}</p>
-            <p><strong>Room:</strong> {patientFull.room}</p>
-            <p><strong>Operating Theater:</strong> {patientFull.operating_theater}</p>
-            <p><strong>Mandatory:</strong> {patientFull.mandatory ? "Yes" : "No"}</p>
-            <p><strong>Gender:</strong> {patientFull.gender}</p>
-            <p><strong>Age Group:</strong> {patientFull.age_group}</p>
-            <p><strong>Length of Stay:</strong> {patientFull.length_of_stay}</p>
-            <p><strong>Surgery Release Day:</strong> {patientFull.surgery_release_day}</p>
-            <p><strong>Surgery Due Day:</strong> {patientFull.surgery_due_day}</p>
-            <p><strong>Surgery Duration:</strong> {patientFull.surgery_duration}</p>
-            <p><strong>Surgeon ID:</strong> {patientFull.surgeon_id}</p>
-            <p>
+            <h3 className='text-center'>{patientId} Details</h3>
+            <p className='text-left'>
+                <strong>Admission Day:</strong> {patientFull.admission_day}
+            </p>
+            <p className='text-left'>
+                <strong>Room:</strong> {patientFull.room}
+            </p>
+            <p className='text-left'>
+                <strong>Operating Theater:</strong> {patientFull.operating_theater}
+            </p>
+            <p className='text-left'>
+                <strong>Mandatory:</strong> {patientFull.mandatory ? "Yes" : "No"}
+            </p>
+            <p className='text-left'>
+                <strong>Gender:</strong> {patientFull.gender}
+            </p>
+            <p className='text-left'>
+                <strong>Age Group:</strong> {patientFull.age_group}
+            </p>
+            <p className='text-left'>
+                <strong>Length of Stay:</strong> {patientFull.length_of_stay}
+            </p>
+            <p className='text-left'>
+                <strong>Surgery Release Day:</strong> {patientFull.surgery_release_day}
+            </p>
+            <p className='text-left'>
+                <strong>Surgery Due Day:</strong> {patientFull.surgery_due_day}
+            </p>
+            <p className='text-left'>
+                <strong>Surgery Duration:</strong> {patientFull.surgery_duration}
+            </p>
+            <p className='text-left'>
+                <strong>Surgeon ID:</strong> {patientFull.surgeon_id}
+            </p>
+            <p className='text-left'>
                 <strong>Incompatible Room IDs:</strong>{" "}
                 {patientFull.incompatible_room_ids.join(', ')}
             </p>
-            {/* <p>
-                <strong>Workload Produced:</strong>{" "}
-                {patientFull.workload_produced.join(', ')}
-            </p>
-            <p>
-                <strong>Skill Level Required:</strong>{" "}
-                {patientFull.skill_level_required.join(', ')}
-            </p> */}
+            {/* Comentado: otros datos */}
         </div>
     );
 };
