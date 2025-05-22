e0db642d1ef07ab473966d23b2ecd9e076e9f5d8
Néstor Feliciano <alu0101488998@ull.edu.es>
2025-05-22 14:51:47 +0100

    Logo

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from './Home.module.scss'
 import backgroundImage from '../../assets/home.png'
 import { InputFiles } from '../../components/InputFiles'
+import logo from '../../../public/logo.png'
 
 export const Home: React.FC = () => (
     <div className={styles.container}>
         <div className={styles.leftPane}>
-            <h1 className={styles.heading}>HealthPlanify</h1>
+            <img src={logo} alt="HealthPlanify logo" width={400} height={400} />
+            {/* <h1 className={styles.heading}>HealthPlanify</h1> */}
             <h2 className={styles.subheading}>A healthcare timetable visualizer</h2>
             <InputFiles />
         </div>
